8af357ae46d522d8a95c67ec33674d93318f2416
Markku Rossi <mtr@iki.fi>
2009-12-30 10:49:10 +0200

    Updated license to GPLv3.

--- gnuincludes.h
+++ gnuincludes.h
@@ -4,7 +4,7 @@
  *
  * Author: Markku Rossi <mtr@iki.fi>
  *
- * Copyright (c) 2001-2002 Markku Rossi.
+ * Copyright (c) 2001-2009 Markku Rossi.
  *
  * What is this file for?
  *
@@ -13,7 +13,7 @@
 /*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
+ * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful,
