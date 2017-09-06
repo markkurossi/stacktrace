7a04bbb829278339358482001521522e605a9617
Markku Rossi <mtr@iki.fi>
2017-09-06 10:02:57 +0300

    Check also PC validity when extracting stack trace.

--- pistacktrace_x86_gcc.c
+++ pistacktrace_x86_gcc.c
@@ -4,7 +4,7 @@
  *
  * Author: Markku Rossi <mtr@iki.fi>
  *
- * Copyright (c) 2003-2016 Markku Rossi.
+ * Copyright (c) 2003-2017 Markku Rossi.
  *
  * See the LICENSE file for the details on licensing.
  *
@@ -44,6 +44,9 @@ while (0)
 /* Check whether the frame pointer `fp' is valid. */
 #define PI_VALID_FP(fp) ((fp) >= (void *) 0x10000000)
 
+/* Check whether the program counter `pc' is valid. */
+#define PI_VALID_PC(pc) ((pc) >= (void *) 0x10000000)
+
 /* Get the next frame pointer from the frame pointer `fp'. */
 #define PI_FP_GET_FP(fp) (*((void **) (fp)))
 
@@ -64,7 +67,11 @@ pi_stack_trace_set(void **stack_trace, PiUInt32 stack_trace_depth)
   for (i = 0;
        PI_VALID_FP(fp) && i < stack_trace_depth;
        fp = PI_FP_GET_FP(fp), i++)
-    stack_trace[i] = PI_FP_GET_PC(fp);
+    {
+      stack_trace[i] = PI_FP_GET_PC(fp);
+      if (!PI_VALID_PC(stack_trace[i]))
+        break;
+    }
 
   return i;
 }
